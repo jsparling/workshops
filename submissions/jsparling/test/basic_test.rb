5f369a67e4b69f581c7ae20b92eba17df38a0ec2
Jeff Ching <ching.jeff@gmail.com>
2015-03-03 20:00:10 -0800

    tests for rules 1 and 2

--- submissions/jsparling/test/basic_test.rb
+++ submissions/jsparling/test/basic_test.rb
@@ -2,6 +2,37 @@ require 'test_helper'
 
 class BasicTest < Minitest::Test
 
+  def test_rule_one
+    # no live neighbors
+    strategy = GameOfLife::BoolStrategy.new([
+      [false, false, false],
+      [false, true, false],
+      [false, false, false]
+    ])
+    strategy.update
+    assert !strategy.cells[1][1]
+
+    # one live neighbor
+    strategy = GameOfLife::BoolStrategy.new([
+      [false, false, false],
+      [false, true, true],
+      [false, false, false]
+    ])
+    strategy.update
+    assert !strategy.cells[1][1]
+  end
+
+  def test_rule_two
+    # 4 live neighbors
+    strategy = GameOfLife::BoolStrategy.new([
+      [true, true, true],
+      [true, true, false],
+      [false, false, false]
+    ])
+    strategy.update
+    assert !strategy.cells[1][1]
+  end
+
   def test_basic
     assert true
   end
