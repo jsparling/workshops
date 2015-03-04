5e1210faa932674e9d87abb20ac118aa27cf4bd0
Jake Sparling <jsparling@avvo.com>
2015-03-03 20:37:45 -0800

    runs

--- submissions/jsparling/bin/run.rb
+++ submissions/jsparling/bin/run.rb
@@ -3,5 +3,16 @@ $:.unshift File.expand_path('../../lib', __FILE__)
 
 require 'game_of_life'
 
-window = GameWindow.new BoolAdaptor.new GameOfLife::BoolStrategy.new
+board = []
+row_count = 100
+col_count = 100
+row_count.times do
+  row = []
+  col_count.times do
+    row << [true, false].sample
+  end
+  board << row
+end
+
+window = GameWindow.new BoolAdaptor.new GameOfLife::BoolStrategy.new(board)
 window.show
